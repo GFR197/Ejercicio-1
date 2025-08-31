13188abd9ef5ac839c27dc5a5b7ee8662e2e2901
GFR197 <gfelix.robles02@gmail.com>
2025-08-30 20:32:38 -0700

    Create Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,7 @@
+# Declaramos una lista de 5 enteros
+numeros = [10, 20, 30, 40, 50]
+
+# Mostramos los elementos de la lista
+print("Elementos del arreglo:")
+for i in range(5):
+    print(f"numeros[{i}] = {numeros[i]}")
