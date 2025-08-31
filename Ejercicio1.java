48b9f681104aa68132a57408250fe0b38303ff3c
GFR197 <gfelix.robles02@gmail.com>
2025-08-30 23:02:42 -0700

    Create Ejercicio1.java

---
+++ Ejercicio1.java
@@ -0,0 +1,14 @@
+public class Ejercicio1 {
+    public class ArregloEnteros {
+    public static void main(String[] args) {
+        // Declaramos un arreglo de 5 enteros
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        // Mostramos los elementos del arreglo
+        System.out.println("Elementos del arreglo:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("numeros[" + i + "] = " + numeros[i]);
+        }
+    }
+}
+}
