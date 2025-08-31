dab831d8e0d3daea6d167b9dfb74dd1a50197aa7
GFR197 <gfelix.robles02@gmail.com>
2025-08-31 00:31:45 -0700

    Create Program.cs
    Ejercicio 1 C#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declaramos un arreglo de 5 enteros
+        int[] numeros = { 10, 20, 30, 40, 50 };
+
+        // Mostramos los elementos del arreglo
+        Console.WriteLine("Elementos del arreglo:");
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.WriteLine($"numeros[{i}] = {numeros[i]}");
+        }
+    }
+}
