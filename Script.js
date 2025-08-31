73538903fa7fa3ae981efd1117cf534e34dae31f
GFR197 <gfelix.robles02@gmail.com>
2025-08-31 00:48:43 -0700

    Create Script.js
    Codigo Ejercicio 1 JavaScript

---
+++ Script.js
@@ -0,0 +1,8 @@
+// Declaramos un arreglo de 5 enteros
+const numeros = [10, 20, 30, 40, 50];
+
+// Mostramos los elementos del arreglo
+console.log("Elementos del arreglo:");
+for (let i = 0; i < numeros.length; i++) {
+    console.log(`numeros[${i}] = ${numeros[i]}`);
+}
